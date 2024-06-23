2f6ea3025e5679078e814fa7fcf34be29bacb4e2
Nakorn Sinpadung <nakorndev@gmail.com>
2024-06-23 09:16:54 +0700

    wip: update layout, add generic buttons component

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,14 @@
 import { z, defineCollection } from 'astro:content'
 
+const urlLinksSchema = z.object({
+  youtube: z.string().url(),
+  docs: z.string().url().optional(),
+  other: z.array(z.object({
+    label: z.string(),
+    url: z.string()
+  })).optional()
+})
+
 export const collections = {
   episodes: defineCollection({
     type: 'content',
@@ -7,7 +16,9 @@ export const collections = {
       title: z.string(),
       tags: z.array(z.string()),
       image: z.string(),
-      previewImages: z.array(z.string())
+      description: z.string(),
+      previewImages: z.array(z.string()),
+      url: urlLinksSchema
     })
   }),
   courses: defineCollection({
@@ -17,14 +28,7 @@ export const collections = {
       tags: z.array(z.string()),
       image: z.string(),
       description: z.string(),
-      url: z.object({
-        youtube: z.string().url(),
-        docs: z.string().url().optional(),
-        other: z.array(z.object({
-          label: z.string(),
-          url: z.string()
-        })).optional()
-      })
+      url: urlLinksSchema
     })
   })
 }
